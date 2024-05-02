6f745371738f8c35fa7e7f136cf7168078013b8c
JatinDhamija816 <jdhamija816@gmail.com>
2024-05-02 11:30:29 +0530

    initial

---
+++ server/src/Database/index.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const ConnectDb = async () => {
+    try {
+        const URL: string = process.env.MONGODB_URI
+        await mongoose.connect(URL)
+        console.log("MongoDB Connected")
+    } catch (error) {
+        console.log("MongoDB Connection Error", error)
+    }
+}
+
+export default ConnectDb
---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import ConnectDb from './Database/index'
+import app from './app'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+
+ConnectDb()
+    .then(() => {
+        app.listen((PORT), () => {
+            console.log("Server Start ")
+        })
+    })
+    .catch((err) => {
+        console.log("Error in index.ts file")
+    })
