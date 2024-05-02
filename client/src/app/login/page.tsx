af941c7e676907793f164093504199fc70f350be
JatinDhamija816 <jdhamija816@gmail.com>
2024-05-02 15:34:19 +0530

    commit

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { MouseEventHandler, useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 
 export default function Login() {
     const [user, setUser] = useState({ email: '', password: '' })
     const router = useRouter()
-    const onLogin = async (e: any) => {
+    const onLogin = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         try {
             if (!user.email || !user.password) return alert('Please Provide all details')
@@ -24,24 +24,26 @@ export default function Login() {
                 <h1 className="font-bold text-3xl my-5">Login</h1>
             </div>
             <div>
-                <div className="m-2">
-                    <label className="ml-1">Email</label><br />
-                    <input type="email" placeholder="Enter Email" value={user.email}
-                        onChange={(e) => setUser({ ...user, email: e.target.value })}
-                        className="border p-1 my-1 rounded-lg outline-none" />
-                </div>
-                <div className="m-2">
-                    <label className="ml-1">Password</label><br />
-                    <input type="password" placeholder="Enter Password" value={user.password}
-                        onChange={(e) => setUser({ ...user, password: e.target.value })}
-                        className="border p-1 my-1 rounded-lg outline-none" />
-                </div>
-                <div className="m-2 my-5 flex justify-center">
-                    <button onClick={onLogin} className="bg-black text-white w-2/3 rounded-lg py-1">Login</button>
-                </div>
-                <div className="m-2">
-                    <Link href='/register'><h1 className="text-center hover:bg-slate-300 rounded-lg py-1">For Register, click here</h1></Link>
-                </div>
+                <form action="" onSubmit={onLogin}>
+                    <div className="m-2">
+                        <label className="ml-1">Email</label><br />
+                        <input type="email" placeholder="Enter Email" value={user.email}
+                            onChange={(e) => setUser({ ...user, email: e.target.value })}
+                            className="border p-1 my-1 rounded-lg outline-none" />
+                    </div>
+                    <div className="m-2">
+                        <label className="ml-1">Password</label><br />
+                        <input type="password" placeholder="Enter Password" value={user.password}
+                            onChange={(e) => setUser({ ...user, password: e.target.value })}
+                            className="border p-1 my-1 rounded-lg outline-none" />
+                    </div>
+                    <div className="m-2 my-5 flex justify-center">
+                        <button className="bg-black text-white w-2/3 rounded-lg py-1">Login</button>
+                    </div>
+                    <div className="m-2">
+                        <Link href='/register'><h1 className="text-center hover:bg-slate-300 rounded-lg py-1">For Register, click here</h1></Link>
+                    </div>
+                </form>
             </div>
         </div >
     );
--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/navigation";
 export default function Register() {
     const [user, setUser] = useState({ username: '', email: '', password: '' })
     const router = useRouter()
-    const onSignup = async (e: any) => {
+    const onSignup = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         try {
             if (!user.username || !user.email || !user.password) return alert('Please Provide all details')
@@ -24,31 +24,33 @@ export default function Register() {
                 <h1 className="font-bold text-3xl my-5">Sign Up</h1>
             </div>
             <div>
-                <div className="m-2">
-                    <label className="ml-1">Username</label><br />
-                    <input type="text" required placeholder="Enter Username" value={user.username}
-                        onChange={(e) => setUser({ ...user, username: e.target.value })}
-                        className="border p-1 my-1 rounded-lg outline-none" />
-                </div>
-                <div className="m-2">
-                    <label className="ml-1">Email</label><br />
-                    <input type="email" required placeholder="Enter Email" value={user.email}
-                        onChange={(e) => setUser({ ...user, email: e.target.value })}
-                        className="border p-1 my-1 rounded-lg outline-none" />
-                </div>
-                <div className="m-2">
-                    <label className="ml-1">Password</label><br />
-                    <input type="password" required placeholder="Enter Password" value={user.password}
-                        onChange={(e) => setUser({ ...user, password: e.target.value })}
-                        className="border p-1 my-1 rounded-lg outline-none" />
-                </div>
-                <div className="m-2 my-5 flex justify-center">
-                    <button onClick={onSignup} className="bg-black text-white w-2/3 rounded-lg py-1">Signup</button>
-                </div>
-                <div className="m-2">
-                    <Link href='/login'><h1 className="text-center hover:bg-slate-300 rounded-lg py-1">For Login, click here</h1></Link>
-                </div>
-            </div>
+                <form action="" onSubmit={onSignup}>
+                    <div className="m-2">
+                        <label className="ml-1">Username</label><br />
+                        <input type="text" required placeholder="Enter Username" value={user.username}
+                            onChange={(e) => setUser({ ...user, username: e.target.value })}
+                            className="border p-1 my-1 rounded-lg outline-none" />
+                    </div>
+                    <div className="m-2">
+                        <label className="ml-1">Email</label><br />
+                        <input type="email" required placeholder="Enter Email" value={user.email}
+                            onChange={(e) => setUser({ ...user, email: e.target.value })}
+                            className="border p-1 my-1 rounded-lg outline-none" />
+                    </div>
+                    <div className="m-2">
+                        <label className="ml-1">Password</label><br />
+                        <input type="password" required placeholder="Enter Password" value={user.password}
+                            onChange={(e) => setUser({ ...user, password: e.target.value })}
+                            className="border p-1 my-1 rounded-lg outline-none" />
+                    </div>
+                    <div className="m-2 my-5 flex justify-center">
+                        <button className="bg-black text-white w-2/3 rounded-lg py-1">Signup</button>
+                    </div>
+                    <div className="m-2">
+                        <Link href='/login'><h1 className="text-center hover:bg-slate-300 rounded-lg py-1">For Login, click here</h1></Link>
+                    </div>
+                </form>
+            </div >
         </div >
     );
 }
