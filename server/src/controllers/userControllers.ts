35c89bc629de32acfdd37078a018e6af9307d0c5
JatinDhamija816 <jdhamija816@gmail.com>
2024-05-04 14:28:41 +0530

    commit

--- server/src/controllers/userControllers.ts
+++ server/src/controllers/userControllers.ts
@@ -2,29 +2,31 @@ import User from "../models/userModel"
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import { Request, Response } from "express"
+import { sendEmail } from '../helper/mailer'
 
 export const RegisterUser = async (req: Request, res: Response) => {
     try {
         const { username, email, password } = req.body
         if (!username || !email || !password) {
-            return res.status(400).send({
+            return res.status(400).json({
                 success: false, message: 'Please fill all details'
             })
         }
         const user = await User.findOne({ email })
         if (user) {
-            return res.status(401).send({
+            return res.status(401).json({
                 success: false, message: 'User already exists'
             })
         }
         const hashedPassword = await bcrypt.hash(password, 10)
         const newUser = new User({ username, email, password: hashedPassword })
         newUser.save()
-        return res.status(201).send({
+        await sendEmail({ email, emailType: "VERIFY", userId: newUser._id })
+        return res.status(201).json({
             success: true, message: 'New user Created', newUser
         })
     } catch (error) {
-        return res.status(500).send({
+        return res.status(500).json({
             message: "Error in Register", success: false,
         })
     }
@@ -34,19 +36,19 @@ export const LoginUser = async (req: Request, res: Response) => {
     try {
         const { email, password } = req.body
         if (!email || !password) {
-            return res.status(400).send({
+            return res.status(400).json({
                 success: false, message: 'Please fill all details'
             })
         }
         const user = await User.findOne({ email })
         if (!user) {
-            return res.status(401).send({
+            return res.status(401).json({
                 success: false, message: 'Email is not Register'
             })
         }
         const isMatch = await bcrypt.compare(password, user.password)
         if (!isMatch) {
-            return res.status(401).send({
+            return res.status(401).json({
                 success: false, message: 'Invalid Email or Password'
             })
         }
@@ -61,7 +63,7 @@ export const LoginUser = async (req: Request, res: Response) => {
             message: "Logged in successfully", success: true, user, token
         })
     } catch (error) {
-        return res.status(500).send({
+        return res.status(500).json({
             message: "Error in Login",
             success: false,
         })
@@ -75,14 +77,14 @@ export const Logout = async (req: Request, res: Response) => {
             success: true
         })
     } catch (error) {
-        return res.status(500).send({
+        return res.status(500).json({
             message: "Error in logout",
             error
         })
     }
 }
 
-function getDataFromToken(req: any) {
+function getDataFromToken(req: Request) {
     try {
         const token = req.cookies.token
         const decodedToken: any = jwt.verify(token, process.env.TOKEN_SECRET!);
@@ -92,7 +94,7 @@ function getDataFromToken(req: any) {
     }
 }
 
-export const userProfile = async (req: any, res: Response) => {
+export const userProfile = async (req: Request, res: Response) => {
     try {
         const userId = await getDataFromToken(req);
         const user = await User.findOne({ _id: userId }).select("-password");
@@ -102,5 +104,27 @@ export const userProfile = async (req: any, res: Response) => {
         })
     } catch (error: any) {
         return res.json({ error: error.message, status: 400 });
+    }
+}
+
+export const VerifyEmail = async (req: Request, res: Response) => {
+    try {
+        const { token } = req.body
+        const user = await User.findOne({ verifyToken: token, verifyTokenExpiry: { $gt: Date.now } })
+        if (!user) {
+            return res.status(400).json({
+                message: 'Invalid token', success: false
+            })
+        }
+        user.isVerfied = true
+        user.verifyToken = undefined
+        user.verifyTokenExpiry = undefined
+        console.log(user)
+        await user.save()
+        return res.json({
+            message: 'Email Verified successfully', success: true
+        })
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
     }
 }
--- server/src/routes/userRoute.ts
+++ server/src/routes/userRoute.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { LoginUser, Logout, RegisterUser, userProfile } from "../controllers/userControllers";
+import { LoginUser, Logout, RegisterUser, userProfile, VerifyEmail } from "../controllers/userControllers";
 
 const router = express()
 
 router.post('/register', RegisterUser)
+router.post('/verifyemail', VerifyEmail)
 router.post('/login', LoginUser)
 
 router.get('/logout', Logout)
